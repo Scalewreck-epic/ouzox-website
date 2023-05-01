3350b674fbc2ea751875b52178d5f403ec1ab289
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-04-30 22:32:58 -0500

    Update upload.js

--- jsbin/upload.js
+++ jsbin/upload.js
@@ -1,9 +1,12 @@
 const filter_api_url = "https://x8ki-letl-twmt.n7.xano.io/api:oyF_ptYd/filter";
 const image_filter_api_url = "https://x8ki-letl-twmt.n7.xano.io/api:oyF_ptYd/image_filter";
 
+const upload_product_api_url = "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E/products";
+const set_product_price_url = "https://x8ki-letl-twmt.n7.xano.io/api:tFdG2Vz-/prices";
+
 const uploadGame = document.getElementById("upload-game");
 
-function filter(text) {
+async function filter(text) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -15,34 +18,35 @@ function filter(text) {
         })
     }
 
-    return fetch(filter_api_url, requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            var result_parse = JSON.parse(result);
+    try {
+        const response = await fetch(filter_api_url, requestOptions);
+        const result = await response.text();
 
-            var response = result_parse.response;
-            var response_result = response.result;
-            var result_response = response_result.response;
+        var result_parse = JSON.parse(result);
+        var response_result = result_parse.response.result;
+        var result_response = response_result.response;
 
-            if (result_response.categories) {
-                for (let i = 0; i < result_response.categories.length; i++) {
-                    const category = result_response.categories[i];
-                    const label = category.label;
-                    const label_topic = label.substring(0, label.indexOf(">"));
-                    const label_reason = label.substring(label.indexOf(">") + 1);
+        if (result_response.categories) {
+            for (let i = 0; i < result_response.categories.length; i++) {
+                const category = result_response.categories[i];
+                const label = category.label;
+                const label_topic = label.substring(0, label.indexOf(">"));
+                const label_reason = label.substring(label.indexOf(">") + 1);
 
-                    if (label_topic == "Sensitive Topics") {
-                        return label_reason;
-                    }
+                if (label_topic == "Sensitive Topics") {
+                    return label_reason;
                 }
             }
+        }
 
-            return "No reason";
-        })
+        return "No reason";
+    } catch (error) {
+        console.warn("There was an error trying to fetch text filter: " , error);
+        return "No reason";
+    }
 }
-  
 
-uploadGame.addEventListener("submit", function(event) {
+uploadGame.addEventListener("submit", async function(event) {
     event.preventDefault();
     var error_label = document.getElementById("error-label");
 
@@ -61,30 +65,141 @@ uploadGame.addEventListener("submit", function(event) {
         const thumbnail_input = document.getElementById("thumbnail");
         const description_input = document.getElementById("description");
         const price_input = document.getElementById("price");
+        const currency_input = document.getElementById("currency-sort");
         const title_input = document.getElementById("title");
 
         function handleFilterResult(result, label) {
             if (result == "No reason") {
                 console.log(label , "accepted through filter.");
+                return true;
             } else {
                 console.warn("Cannot continue upload process because text includes" , result);
                 error_label.innerHTML = label+" not accepted because of "+result;
-                throw new Error("Upload process cancelled.");
+                return false;
             }
         }
-        
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const currency = currency_input.options[currency_input.selectedIndex].value;
+
+        var uploadRequestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            redirect: "follow",
+            body: JSON.stringify({
+                "product": {
+                    "id": null,
+                    "name": title_input.value,
+                    "active": null,
+                    "description": description_input.value,
+                    "metadata": null,
+                    "type": null,
+                    "attributes": [],
+                    "caption": null,
+                    "deactivate_on": [],
+                    "images": [],
+                    "package_dimensions": {
+                      "height": null,
+                      "length": null,
+                      "weight": null,
+                      "width": null
+                    },
+                    "shippable": null,
+                    "statement_descriptor": null,
+                    "unit_label": null,
+                    "url": null
+                }
+            })
+        };
+
+        async function uploadProduct() {
+            try {
+                const response = await fetch(upload_product_api_url, uploadRequestOptions);
+                const result = await response.text();
+                const result_parse = JSON.parse(result);
+    
+                console.log(result_parse);
+                return result_parse;
+            } catch (error) {
+                warn("There was an error trying to upload a product: "+error);
+            }
+        }
+
+        async function setProductPrice(product_id) {
+            var priceRequestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                redirect: "follow",
+                body: JSON.stringify({
+                    "price": {
+                        "currency": currency,
+                        "unit_amount": price_input.value * 100,
+                        "active": null,
+                        "nickname": "",
+                        "product": product_id,
+                        "recurring": {
+                          "interval": null,
+                          "aggregate_usage": null,
+                          "interval_count": null,
+                          "usage_type": null
+                        },
+                        "tiers": [],
+                        "tiers_mode": null,
+                        "billing_scheme": null,
+                        "lookup_key": null,
+                        "product_data": {
+                          "name": null,
+                          "active": null,
+                          "statement_descriptor": null,
+                          "unit_label": null,
+                          "metadata": null
+                        },
+                        "transfer_lookup_key": null,
+                        "transform_quantity": {
+                          "divide_by": null,
+                          "round": null
+                        },
+                        "unit_amount_decimal": null,
+                        "metadata": null
+                    }
+                })
+            }
+
+            try {
+                const response = await fetch(set_product_price_url, priceRequestOptions);
+                const result = await response.text();
+                const result_parse = JSON.parse(result);
+    
+                console.log(result_parse);
+            } catch (error) {
+                warn("There was an error trying to set price: "+error);
+            }
+        }
+
         try {
-            filter(title_input.value).then(result => {
-                handleFilterResult(result, "Title");
-                filter(description_input.value).then(result => {
-                    handleFilterResult(result, "Description");
-                });
-            });
+            const titleResult = await filter(title_input.value);
+            const isTitleValid = handleFilterResult(titleResult, "Title");
+        
+            const descriptionResult = await filter(description_input.value);
+            const isDescriptionValid = handleFilterResult(descriptionResult, "Description");
+        
+            // Check if both title and description are accepted before uploading the product
+            if (isTitleValid && isDescriptionValid) {
+                const result = await uploadProduct();
+                if (result) {
+                    await setProductPrice(result.id);
+                    console.log("Product uploaded successfully!");
+                } else {
+                    error_label.innerHTML = "There was an error trying to upload game.";
+                }
+            } else {
+                error_label.innerHTML = "Incomplete form.";
+            }
         } catch(error) {
             console.warn("There was an error trying to handle text filter: " , error);
         }
-    } else {
-        error_label.innerHTML = "Incomplete form.";
     }
 });
 
@@ -116,15 +231,6 @@ function checkFileSize() {
     } else {
         warn.innerHTML = "";
     }
-}
-
-function updatePriceLabel() {
-    const currencySort = document.getElementById("currency-sort");
-    const priceLabel = document.getElementById("price-label");
-
-    const currencyOption = currencySort.options[currencySort.selectedIndex];
-    const currencySymbol = currencyOption.textContent.match(/\((.*)\)/)[1];
-    priceLabel.innerHTML = "Price (" + currencySymbol + "):";
 }
 
 function checkIsFree() {
@@ -187,7 +293,6 @@ function checkPrice() {
 }
 
 const currencySort = document.getElementById("currency-sort");
-currencySort.addEventListener("change", updatePriceLabel);
 
 checkDescriptionLength();
 checkTitleLength();
