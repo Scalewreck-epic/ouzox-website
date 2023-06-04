4cf9c05890fd8c6e49d4d9962fe6e8ee92593797
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-06-04 15:07:00 -0500

    Update data.js

--- jsbin/data.js
+++ jsbin/data.js
@@ -1,6 +1,11 @@
 const games_list_api = "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E:v1/products";
 const games_prices_url = "https://x8ki-letl-twmt.n7.xano.io/api:tFdG2Vz-/prices";
 
+const update_product_url = "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E/products/"; // + product id
+
+const get_user_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv/user/"; // + session id
+const filter_api_url = "https://x8ki-letl-twmt.n7.xano.io/api:oyF_ptYd/filter";
+
 const refreshTime = 5;
 const gamesPerPage = 20;
 var isFetching = false;
@@ -12,7 +17,7 @@ var games = [];
 function getGamePrice(game_id) {
     const result = prices.find(item => item.product === game_id);
     if (result) {
-        return { price: result.unit_amount, currency: result.currency };
+        return { price: result.unit_amount, currency: result.currency, id: result.id };
     };
 };
 
@@ -25,6 +30,46 @@ function calculateDiffDays(timestamp) {
 
     return createdDiffDays;
 };
+
+async function filter(text) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify({
+            "text": text,
+        })
+    }
+
+    try {
+        const response = await fetch(filter_api_url, requestOptions);
+        const result = await response.text();
+
+        var result_parse = JSON.parse(result);
+        var response_result = result_parse.response.result;
+        var result_response = response_result.response;
+
+        if (result_response.categories) {
+            for (let i = 0; i < result_response.categories.length; i++) {
+                const category = result_response.categories[i];
+                const label = category.label;
+                const label_topic = label.substring(0, label.indexOf(">"));
+                const label_reason = label.substring(label.indexOf(">") + 1);
+
+                if (label_topic == "Sensitive Topics") {
+                    return label_reason;
+                }
+            }
+        }
+
+        return "No reason";
+    } catch (error) {
+        console.warn("There was an error trying to fetch text filter: " , error);
+        return "No reason";
+    }
+}
 
 function createGamePage(game, game_price, editable) {
     var price = game_price.price / 100;
@@ -102,16 +147,82 @@ function createGamePage(game, game_price, editable) {
 
     if (editable) {
         gameTitle.contentEditable = true;
-        gamePrice.contentEditable = true;
 
         var deleteButton = document.createElement("button");
         deleteButton.className = "delete-button";
         deleteButton.innerHTML = "DELETE";
-        gamesDiv.appendChild(deleteButton);
 
         var commitChangesButton = document.createElement("button");
         commitChangesButton.className = "delete-button";
         commitChangesButton.innerHTML = "COMMIT CHANGES";
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        deleteButton.addEventListener("click", async function() {
+            var deactivate_product_options = {
+                method: "POST",
+                headers: myHeaders,
+                redirect: "follow",
+                body: JSON.stringify({
+                    "product": {
+                        "active": "false",
+                  },
+                  "id": game.id,
+                })
+            }
+
+            async function deactivate_product() {
+                try {
+                    const response = await fetch(update_product_url + game.id, deactivate_product_options);
+                    const result = await response.text();
+                    const result_parse = JSON.parse(result);
+    
+                    console.log(result_parse);
+                } catch (error) {
+                    showError(error, false);
+                };
+            };
+
+            await deactivate_product();
+            gamesDiv.remove();
+        });
+
+        commitChangesButton.addEventListener("click", async function() {
+            const filterText = filter(gameTitle.innerHTML);
+
+            if (filterText == "No reason") {
+                var update_product_options = {
+                    method: "POST",
+                    headers: myHeaders,
+                    redirect: "follow",
+                    body: JSON.stringify({
+                        "product": {
+                          "name": gameTitle.innerHTML,
+                        },
+                        "id": game.id,
+                    })
+                }
+    
+                async function update_product() {
+                    try {
+                        const response = await fetch(update_product_url + game.id, update_product_options);
+                        const result = await response.text();
+                        const result_parse = JSON.parse(result);
+        
+                        console.log(result_parse);
+                    } catch (error) {
+                        showError(error, false);
+                    };
+                };
+    
+                await update_product();
+            } else {
+                gameTitle.innerHTML = filterText;
+            }
+        })
+
+        gamesDiv.appendChild(deleteButton);
         gamesDiv.appendChild(commitChangesButton);
     }
 
@@ -140,6 +251,49 @@ function sortGames(gameSortType, listSortType) {
     }
 }
 
+function getCookieData(trim) {
+    const cookies = document.cookie;
+    const cookieArray = cookies.split(";");
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        const cookie = cookieArray[i];
+        const [name, value] = cookie.split("=");
+        if (name.trim() === trim) {
+            return {
+                "Data": value.toString(),
+                "Valid": true,
+            };
+        }
+    }
+
+    return {
+        "Data": "no data.",
+        "Valid": false,
+    };
+}
+
+async function verifyUser() {
+    var data = getCookieData("session_id");
+
+    if (data.Valid) {
+        async function get_user() {
+            try {
+                const response = await fetch(get_user_url + data.Data, update_product_options);
+                const result = await response.text();
+                const result_parse = JSON.parse(result);
+    
+                console.log(result_parse);
+                return result_parse;
+            } catch (error) {
+                showError(error, false);
+            };
+        };
+    
+        var user = await get_user();
+        return user;
+    }
+}
+
 function loadGames() {
     for (let i = currentPage; i < currentPage + gamesPerPage && i < games.length; i++) {
         var game = games[i];
@@ -156,20 +310,23 @@ function loadGames() {
 };
 
 function loadDashboard() {
-    const username = document.getElementById("username");
+    const username = verifyUser()
 
-    for (let i = currentPage; i < currentPage + gamesPerPage && i < games.length; i++) {
-        var game = games[i];
-
-        if (game && game.active && game.metadata.developer_name == username.innerHTML) {
-            var game_price = getGamePrice((game.id).toString());
-
-            if (game_price) {
-                currentPage += 1;
-                createGamePage(game, game_price, true);
+    if (username) {
+        for (let i = currentPage; i < currentPage + gamesPerPage && i < games.length; i++) {
+            var game = games[i];
+    
+            if (game && game.active && game.metadata.developer_name == username.name) {
+                var game_price = getGamePrice((game.id).toString());
+    
+                if (game_price) {
+                    currentPage += 1;
+                    console.log(game_price);
+                    createGamePage(game, game_price, true);
+                };
             };
         };
-    };
+    }
 }
 
 function showError(err) {
