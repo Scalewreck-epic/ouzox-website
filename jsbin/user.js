ca731cef989c4ad4cecdc8b7e1197b426750173f
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-06-13 13:37:20 -0500

    Update user.js

--- jsbin/user.js
+++ jsbin/user.js
@@ -2,7 +2,7 @@ const signup_endpoint = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/auth/
 const login_endpoint = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/auth/login";
 const getsingle_endpoint = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv/user/" // + user session
 
-import { getCookie } from "./exportuser.js";
+import { getCookie, changeEmailData, changePasswordData } from "./exportuser.js";
 
 const annualExpiration = 1;
 
@@ -212,50 +212,6 @@ function changeSessionData(headers, endpoint) {
             error_label.innerHTML = "Successfully changed settings!";
         }
     })
-}
-
-function changeEmailData() {
-    var data = getCookie("session_id");
-
-    if (data.Valid) {
-        const new_email = document.getElementById("email_input").value;
-
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        var requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify({
-                "session_id": data.Data,
-                "new_email": new_email,
-            }),
-        };
-
-        changeSessionData(requestOptions, ("https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_email/" + data.Data));
-    }
-}
-
-function changePasswordData() {
-    var data = getCookie("session_id");
-
-    if (data.Valid) {
-        const new_password = document.getElementById("password_input").value;
-        const old_password = document.getElementById("old_password_input").value;
-
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        var requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify({
-                "session_id": data.Data,
-                "old_password": old_password,
-                "new_password": new_password,
-            }),
-        };
-        
-        changeSessionData(requestOptions, ("https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_pass/" + data.Data));
-    }
 }
 
 function logout() {
