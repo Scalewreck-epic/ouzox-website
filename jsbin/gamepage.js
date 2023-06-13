5509f5d129580786e58938fa9314995f04d34395
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-06-13 13:24:18 -0500

    Update gamepage.js

--- jsbin/gamepage.js
+++ jsbin/gamepage.js
@@ -1,66 +1,11 @@
 const get_product_url = "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E/products/"; // + product id
-const get_user_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv/user/"; // + session id
-
 const update_product_url = "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E/products/"; // + product id
 
 import {filter} from "./moderation.js";
+import {getUser} from "./exportuser.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 const gameId = urlParams.get("j");
-
-function getCookieData(trim) {
-    const cookies = document.cookie;
-    const cookieArray = cookies.split(";");
-
-    for (let i = 0; i < cookieArray.length; i++) {
-        const cookie = cookieArray[i];
-        const [name, value] = cookie.split("=");
-
-        const cookieName = name.trim();
-
-        if (cookieName === trim) {
-            return {
-                "Data": value.toString(),
-                "Valid": true,
-            };
-        }
-    }
-
-    return {
-        "Data": "no data.",
-        "Valid": false,
-    };
-};
-
-async function getUsername() {
-    var data = getCookieData("session_id");
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var get_user_options = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
-    }
-
-    if (data.Valid) {
-        async function get_user() {
-            try {
-                const response = await fetch(get_user_url + data.Data, get_user_options);
-                const result = await response.text();
-                const result_parse = JSON.parse(result);
-    
-                return result_parse;
-            } catch (error) {
-                showError(error, false);
-            };
-        };
-    
-        var user = await get_user();
-        return user;
-    }
-}
 
 async function retrieveGameData(gameId) {
     var myHeaders = new Headers();
@@ -188,7 +133,7 @@ const gameHandler = async (gameId) => {
         // send user to the stripe payment session
     });
 
-    const user = await getUsername();
+    const user = await getUser();
 
     if (user.name == gameData.developer_name) {
         game_title.contentEditable = true;
