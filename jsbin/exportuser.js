b328bbd703b31390f8f3cf17bb88a5d7bcbe28b8
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-06-13 13:37:10 -0500

    Update exportuser.js

--- jsbin/exportuser.js
+++ jsbin/exportuser.js
@@ -1,4 +1,6 @@
 const get_user_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv/user/"; // + session id
+const change_user_email_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_email/"; // + session id
+const change_user_password_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_pass/"; // + session id
 
 export async function getCookie(trim) {
     const cookies = document.cookie;
@@ -23,6 +25,50 @@ export async function getCookie(trim) {
         "Valid": false,
     };
 };
+
+export async function changeEmailData() {
+    var data = getCookie("session_id");
+
+    if (data.Valid) {
+        const new_email = document.getElementById("email_input").value;
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify({
+                "session_id": data.Data,
+                "new_email": new_email,
+            }),
+        };
+
+        changeSessionData(requestOptions, (change_user_email_url + data.Data));
+    }
+}
+
+export async function changePasswordData() {
+    var data = getCookie("session_id");
+
+    if (data.Valid) {
+        const new_password = document.getElementById("password_input").value;
+        const old_password = document.getElementById("old_password_input").value;
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify({
+                "session_id": data.Data,
+                "old_password": old_password,
+                "new_password": new_password,
+            }),
+        };
+        
+        changeSessionData(requestOptions, (change_user_password_url + data.Data));
+    }
+}
 
 export async function getUser() {
     var data = getCookie("session_id");
