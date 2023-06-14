32e3c0b19c08264d2b6161ce6e7cdf0ea2b73740
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-06-13 20:40:58 -0500

    Update gamepage.js

--- jsbin/gamepage.js
+++ jsbin/gamepage.js
@@ -88,187 +88,192 @@ async function retrieveGameData(gameId) {
 }
 
 const gameHandler = async (gameId) => {
-    const gameData = await retrieveGameData(gameId);
+    if (gameId != null) {
+        const gameData = await retrieveGameData(gameId);
 
-    const game_title = document.getElementById("game-title");
-    const game_desc = document.getElementById("game-description");
-    const created = document.getElementById("created");
-    const updated = document.getElementById("updated");
-    const icon = document.getElementById("icon");
-
-    const developer_name = document.getElementById("game-developer-name");
-    const game_genre = document.getElementById("game-genre");
-    const game_summary = document.getElementById("game-summary");
-    const game_art = document.getElementById("game-art")
-
-    // main data
-    game_title.innerHTML = gameData.name;
-    game_desc.innerHTML = gameData.description;
-    created.innerHTML = gameData.created;
-    updated.innerHTML = gameData.updated;
-    icon.setAttribute("href", gameData.icon);
-
-    // metadata
-    developer_name.innerHTML = "By: "+gameData.developer_name;
-    game_genre.innerHTML = gameData.genre;
-    game_summary.innerHTML = gameData.summary;
-    game_art.innerHTML = gameData.artstyle;
-
-    if (!gameData.useDefaultColors) {
-        var elements = document.getElementsByClassName("game-stat");
-
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].style.color = gameData.colors.statsColor;
-        }
-
-        document.body.style.backgroundColor = gameData.colors.bgColor;
-        document.getElementById("game-column").style.backgroundColor = gameData.colors.bg2Color;
-        document.getElementById("game-title-column").style.color = gameData.colors.titleColor;
-        document.getElementById("game-description").style.color = gameData.colors.descColor;
-        document.getElementById("download-button").style.backgroundColor = gameData.colors.buttonColor;
-        document.getElementById("download-button").style.color = gameData.colors.buttonTextColor;
-    }
-
-    document.getElementById("download-button").addEventListener("click", function() {
-        // send user to the stripe payment session
-    });
-
-    const user = await getUser();
-
-    if (user.name == gameData.developer_name) {
-        game_title.contentEditable = true;
-        game_desc.contentEditable = true;
-        game_summary.contentEditable = true;
-
-        var commitChangesButton = document.createElement("button");
-        commitChangesButton.className = "game-download-button";
-        commitChangesButton.innerHTML = "Commit Changes";
-
-        function create_stat(stat_name) {
-            var game_stat = document.createElement("div");
-            game_stat.className = "game-stat";
+        const game_title = document.getElementById("game-title");
+        const game_desc = document.getElementById("game-description");
+        const created = document.getElementById("created");
+        const updated = document.getElementById("updated");
+        const icon = document.getElementById("icon");
     
-            var stat_title = document.createElement("div");
-            stat_title.className = "game-updated-title";
-            stat_title.innerHTML = stat_name;
-            game_stat.appendChild(stat_title);
+        const developer_name = document.getElementById("game-developer-name");
+        const game_genre = document.getElementById("game-genre");
+        const game_summary = document.getElementById("game-summary");
+        const game_art = document.getElementById("game-art")
     
-            var changeBGcolor = document.createElement("input");
-            changeBGcolor.className = "game-download-input";
-            changeBGcolor.type = "color";
-            game_stat.appendChild(changeBGcolor);
-            document.getElementById("game-column").appendChild(game_stat);
-
-            return changeBGcolor;
-        }
-
-        var changeBGcolor = create_stat("BG Color");
-        var changeBG2color = create_stat("BG2 Color");
-        var changeTitleColor = create_stat("Title Color");
-        var changeDescColor = create_stat("Description Color");
-        var changeStatsColor = create_stat("Stats Color");
-        var changeButtonColor = create_stat("Button BG Color");
-        var changeButtonText = create_stat("Button Text Color");
-
-        changeBGcolor.onchange = function() {
-            document.body.style.backgroundColor = changeBGcolor.value;
-        };
-
-        changeBG2color.onchange = function() {
-            document.getElementById("game-column").style.backgroundColor = changeBG2color.value;
-        };
-
-        changeTitleColor.onchange = function() {
-            document.getElementById("game-title-column").style.color = changeTitleColor.value;
-        };
-
-        changeDescColor.onchange = function() {
-            document.getElementById("game-description").style.color = changeDescColor.value;
-        };
-
-        changeButtonColor.onchange = function() {
-            document.getElementById("download-button").style.backgroundColor = changeButtonColor.value;
-        }
-
-        changeButtonText.onchange = function() {
-            document.getElementById("download-button").style.color = changeButtonText.value;
-        }
-
-        changeStatsColor.onchange = function() {
+        // main data
+        game_title.innerHTML = gameData.name;
+        game_desc.innerHTML = gameData.description;
+        created.innerHTML = gameData.created;
+        updated.innerHTML = gameData.updated;
+        icon.setAttribute("href", gameData.icon);
+    
+        // metadata
+        developer_name.innerHTML = "By: "+gameData.developer_name;
+        game_genre.innerHTML = gameData.genre;
+        game_summary.innerHTML = gameData.summary;
+        game_art.innerHTML = gameData.artstyle;
+    
+        if (!gameData.useDefaultColors) {
             var elements = document.getElementsByClassName("game-stat");
-
-            for (var i = 0; i < elements.length; i++) {
-                elements[i].style.color = changeStatsColor.value;
-            }
-        };
-        
-        var isLoading = false;
-        commitChangesButton.addEventListener("click", async function() {
-            if (!isLoading) {
-                isLoading = true;
-                commitChangesButton.innerHTML = "Uploading..";
-                const titleFilter = await filter(game_title.innerHTML);
-                const descFilter = await filter(game_desc.innerHTML);
-                
-                if (titleFilter == "No reason" && descFilter == "No reason") {
-                    const myHeaders = new Headers();
-                    myHeaders.append("Content-Type", "application/json");
-
-                    var update_product_options = {
-                        method: "POST",
-                        headers: myHeaders,
-                        redirect: "follow",
-                        body: JSON.stringify({
-                            "product": {
-                              "name": game_title.innerHTML,
-                              "description": game_desc.innerHTML,
-                              "metadata": {
-                                "summary": game_summary.innerHTML,
-                                "bgColor": document.body.style.backgroundColor,
-                                "bg2Color": document.getElementById("game-column").style.backgroundColor,
-                                "titleColor": document.getElementById("game-title-column").style.color,
-                                "descColor": document.getElementById("game-description").style.color,
-                                "buttonColor": document.getElementById("download-button").style.backgroundColor,
-                                "buttonTextColor": document.getElementById("download-button").style.color,
-                                "statsColor": document.getElementsByClassName("game-stat")[0].style.color,
-                              }
-                            },
-                            "id": game.id,
-                        })
-                    }
-        
-                    async function update_product() {
-                        if (user.name == gameData.developer_name) {
-                            try {
-                                await fetch(update_product_url + game.id, update_product_options);
-                                commitChangesButton.innerHTML = "Success";
-                            } catch (error) {
-                                commitChangesButton.innerHTML = "An error occured";
-                                showError(error, false);
-                            };
-                        } else {
-                            console.warn("User somehow accessed the developer panel onto a project they do not own.");
-                            commitChangesButton.innerHTML = "You should not be editing this project.";
-                        }
-                    };
-        
-                    await update_product();
-                } else {
-                    if (titleFilter != "No reason") {
-                        game_title.innerHTML = titleFilter;
-                    };
     
-                    if (descFilter != "No reason") {
-                        game_desc.innerHTML = descFilter;
-                    };
-                    commitChangesButton.innerHTML = "Cannot upload";
-                }
-                isLoading = false;
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.color = gameData.colors.statsColor;
             }
-        })
-
-        document.getElementById("buttons").appendChild(commitChangesButton);
-    }
+    
+            document.body.style.backgroundColor = gameData.colors.bgColor;
+            document.getElementById("game-column").style.backgroundColor = gameData.colors.bg2Color;
+            document.getElementById("game-title-column").style.color = gameData.colors.titleColor;
+            document.getElementById("game-description").style.color = gameData.colors.descColor;
+            document.getElementById("download-button").style.backgroundColor = gameData.colors.buttonColor;
+            document.getElementById("download-button").style.color = gameData.colors.buttonTextColor;
+        }
+    
+        document.getElementById("download-button").addEventListener("click", function() {
+            // send user to the stripe payment session
+        });
+    
+        const user = await getUser();
+    
+        if (user.name == gameData.developer_name) {
+            game_title.contentEditable = true;
+            game_desc.contentEditable = true;
+            game_summary.contentEditable = true;
+    
+            var commitChangesButton = document.createElement("button");
+            commitChangesButton.className = "game-download-button";
+            commitChangesButton.innerHTML = "Commit Changes";
+    
+            function create_stat(stat_name) {
+                var game_stat = document.createElement("div");
+                game_stat.className = "game-stat";
+        
+                var stat_title = document.createElement("div");
+                stat_title.className = "game-updated-title";
+                stat_title.innerHTML = stat_name;
+                game_stat.appendChild(stat_title);
+        
+                var changeBGcolor = document.createElement("input");
+                changeBGcolor.className = "game-download-input";
+                changeBGcolor.type = "color";
+                game_stat.appendChild(changeBGcolor);
+                document.getElementById("game-column").appendChild(game_stat);
+    
+                return changeBGcolor;
+            }
+    
+            var changeBGcolor = create_stat("BG Color");
+            var changeBG2color = create_stat("BG2 Color");
+            var changeTitleColor = create_stat("Title Color");
+            var changeDescColor = create_stat("Description Color");
+            var changeStatsColor = create_stat("Stats Color");
+            var changeButtonColor = create_stat("Button BG Color");
+            var changeButtonText = create_stat("Button Text Color");
+    
+            changeBGcolor.onchange = function() {
+                document.body.style.backgroundColor = changeBGcolor.value;
+            };
+    
+            changeBG2color.onchange = function() {
+                document.getElementById("game-column").style.backgroundColor = changeBG2color.value;
+            };
+    
+            changeTitleColor.onchange = function() {
+                document.getElementById("game-title-column").style.color = changeTitleColor.value;
+            };
+    
+            changeDescColor.onchange = function() {
+                document.getElementById("game-description").style.color = changeDescColor.value;
+            };
+    
+            changeButtonColor.onchange = function() {
+                document.getElementById("download-button").style.backgroundColor = changeButtonColor.value;
+            }
+    
+            changeButtonText.onchange = function() {
+                document.getElementById("download-button").style.color = changeButtonText.value;
+            }
+    
+            changeStatsColor.onchange = function() {
+                var elements = document.getElementsByClassName("game-stat");
+    
+                for (var i = 0; i < elements.length; i++) {
+                    elements[i].style.color = changeStatsColor.value;
+                }
+            };
+            
+            var isLoading = false;
+            commitChangesButton.addEventListener("click", async function() {
+                if (!isLoading) {
+                    isLoading = true;
+                    commitChangesButton.innerHTML = "Uploading..";
+                    const titleFilter = await filter(game_title.innerHTML);
+                    const descFilter = await filter(game_desc.innerHTML);
+                    
+                    if (titleFilter == "No reason" && descFilter == "No reason") {
+                        const myHeaders = new Headers();
+                        myHeaders.append("Content-Type", "application/json");
+    
+                        var update_product_options = {
+                            method: "POST",
+                            headers: myHeaders,
+                            redirect: "follow",
+                            body: JSON.stringify({
+                                "product": {
+                                  "name": game_title.innerHTML,
+                                  "description": game_desc.innerHTML,
+                                  "metadata": {
+                                    "summary": game_summary.innerHTML,
+                                    "bgColor": document.body.style.backgroundColor,
+                                    "bg2Color": document.getElementById("game-column").style.backgroundColor,
+                                    "titleColor": document.getElementById("game-title-column").style.color,
+                                    "descColor": document.getElementById("game-description").style.color,
+                                    "buttonColor": document.getElementById("download-button").style.backgroundColor,
+                                    "buttonTextColor": document.getElementById("download-button").style.color,
+                                    "statsColor": document.getElementsByClassName("game-stat")[0].style.color,
+                                  }
+                                },
+                                "id": gameId,
+                            })
+                        }
+            
+                        async function update_product() {
+                            if (user.name == gameData.developer_name) {
+                                try {
+                                    await fetch(update_product_url + gameId, update_product_options);
+                                    commitChangesButton.innerHTML = "Success";
+                                } catch (error) {
+                                    commitChangesButton.innerHTML = "An error occured";
+                                    showError(error, false);
+                                };
+                            } else {
+                                console.warn("User somehow accessed the developer panel onto a project they do not own.");
+                                commitChangesButton.innerHTML = "You should not be editing this project.";
+                            }
+                        };
+            
+                        await update_product();
+                    } else {
+                        if (titleFilter != "No reason") {
+                            game_title.innerHTML = titleFilter;
+                        };
+        
+                        if (descFilter != "No reason") {
+                            game_desc.innerHTML = descFilter;
+                        };
+                        commitChangesButton.innerHTML = "Cannot upload";
+                    };
+                    isLoading = false;
+                };
+            });
+    
+            document.getElementById("buttons").appendChild(commitChangesButton);
+        };
+    } else {
+        console.warn("There is no valid game id.");
+        window.location.assign("404.html");
+    };
 }
 
 if (gameId != null) {
