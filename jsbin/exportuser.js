a0af834e9e8a11f24aaa13e461a90b4c7a1f7f57
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-06-21 17:41:15 -0500

    Update exportuser.js

--- jsbin/exportuser.js
+++ jsbin/exportuser.js
@@ -1,120 +1,125 @@
 const get_user_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv/user/"; // + session id
-const change_user_email_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_email/"; // + session id
-const change_user_password_url = "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_pass/"; // + session id
+const change_user_email_url =
+  "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_email/"; // + session id
+const change_user_password_url =
+  "https://x8ki-letl-twmt.n7.xano.io/api:V36A7Ayv:v1/user/edit_pass/"; // + session id
 
 function changeSessionData(headers, endpoint) {
-    var error_label = document.getElementById("error-label");
-    error_label.innerHTML = "Changing settings...";
+  const error_label = document.getElementById("error-label");
+  error_label.innerHTML = "Changing settings...";
 
-    fetch(endpoint, headers)
-    .then(response => response.text())
-    .then(result => {
-        var result_parse = JSON.parse(result);
+  fetch(endpoint, headers)
+    .then((response) => response.text())
+    .then((result) => {
+      const result_parse = JSON.parse(result);
 
-        if (result_parse.message) {
-            error_label.innerHTML = result_parse.message;
-        } else {
-            error_label.innerHTML = "Successfully changed settings!";
-        }
-    })
+      if (result_parse.message) {
+        error_label.innerHTML = result_parse.message;
+      } else {
+        error_label.innerHTML = "Successfully changed settings!";
+      }
+    });
 }
 
 export function getCookie(wanted) {
-    const cookies = document.cookie;
-    const cookieArray = cookies.split(";");
+  const cookies = document.cookie;
+  const cookieArray = cookies.split(";");
 
-    if (cookies != "") {
-        for (let i = 0; i < cookieArray.length; i++) {
-            const cookie = cookieArray[i];
-            const [name, value] = cookie.split("=");
-    
-            const cookieName = name.trim();
-    
-            if (cookieName === wanted) {
-                return {
-                    "Data": value.toString(),
-                    "Valid": true,
-                };
-            };
-        };
-    } else {
+  if (cookies != "") {
+    for (let i = 0; i < cookieArray.length; i++) {
+      const cookie = cookieArray[i];
+      const [name, value] = cookie.split("=");
+
+      const cookieName = name.trim();
+
+      if (cookieName === wanted) {
         return {
-            "Data": "no data.",
-            "Valid": false,
+          Data: value.toString(),
+          Valid: true,
         };
+      }
+    }
+  } else {
+    return {
+      Data: "no data.",
+      Valid: false,
     };
-};
+  }
+}
 
 export async function changeEmailData() {
-    var data = getCookie("session_id");
+  const data = getCookie("session_id");
 
-    if (data.Valid) {
-        const new_email = document.getElementById("email_input").value;
+  if (data.Valid) {
+    const new_email = document.getElementById("email_input").value;
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        var requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify({
-                "session_id": data.Data,
-                "new_email": new_email,
-            }),
-        };
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        session_id: data.Data,
+        new_email: new_email,
+      }),
+    };
 
-        changeSessionData(requestOptions, (change_user_email_url + data.Data));
-    }
+    changeSessionData(requestOptions, change_user_email_url + data.Data);
+  }
 }
 
 export async function changePasswordData() {
-    var data = getCookie("session_id");
+  const data = getCookie("session_id");
 
-    if (data.Valid) {
-        const new_password = document.getElementById("password_input").value;
-        const old_password = document.getElementById("old_password_input").value;
+  if (data.Valid) {
+    const new_password = document.getElementById("password_input").value;
+    const old_password = document.getElementById("old_password_input").value;
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        var requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify({
-                "session_id": data.Data,
-                "old_password": old_password,
-                "new_password": new_password,
-            }),
-        };
-        
-        changeSessionData(requestOptions, (change_user_password_url + data.Data));
-    }
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({
+        session_id: data.Data,
+        old_password: old_password,
+        new_password: new_password,
+      }),
+    };
+
+    changeSessionData(requestOptions, change_user_password_url + data.Data);
+  }
 }
 
 export async function getUser() {
-    var data = getCookie("session_id");
+  const data = getCookie("session_id");
 
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    var get_user_options = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
+  const get_user_options = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  if (data.Valid) {
+    async function get_user() {
+      try {
+        const response = await fetch(
+          get_user_url + data.Data,
+          get_user_options
+        );
+        const result = await response.text();
+        const result_parse = JSON.parse(result);
+
+        return result_parse;
+      } catch (error) {
+        showError(error, false);
+      }
     }
 
-    if (data.Valid) {
-        async function get_user() {
-            try {
-                const response = await fetch(get_user_url + data.Data, get_user_options);
-                const result = await response.text();
-                const result_parse = JSON.parse(result);
-    
-                return result_parse;
-            } catch (error) {
-                showError(error, false);
-            };
-        };
-    
-        var user = await get_user();
-        return user;
-    };
-};
+    const user = await get_user();
+    return user;
+  }
+}
