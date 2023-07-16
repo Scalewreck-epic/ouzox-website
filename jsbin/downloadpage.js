b82c15e5caa9c0de14036d4dbeeb2f22f4cd6047
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-07-16 17:22:24 -0500

    Update downloadpage.js

--- jsbin/downloadpage.js
+++ jsbin/downloadpage.js
@@ -1,46 +1,46 @@
-const get_product_url = "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E/products/"; // + price id
+const get_product_url =
+  "https://x8ki-letl-twmt.n7.xano.io/api:iwAsZq4E/products/"; // + price id
 
 const urlParams = new URLSearchParams(window.location.search);
 const gameIdParam = urlParams.get("g");
 const priceIdParam = urlParams.get("p");
 
 async function setGameData(gameId, priceId) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-  
-    const options = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
-  
-    async function getGameData() {
-      try {
-        const response = await fetch(get_product_url + gameId, options);
-        const result = await response.text();
-        const result_parse = JSON.parse(result);
-  
-        return result_parse;
-      } catch (error) {
-        console.warn("There was an error trying to get game data: ", error);
-        console.warn("Redirecting to 404 error page.");
-        window.location.assign("404.html");
-      }
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const options = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  async function getGameData() {
+    try {
+      const response = await fetch(get_product_url + gameId, options);
+      const result = await response.text();
+      const result_parse = JSON.parse(result);
+
+      return result_parse;
+    } catch (error) {
+      console.error("There was an error trying to get game data: ", error);
+      window.location.assign("404.html");
     }
+  }
 
-    const gameData = await getGameData();
+  const gameData = await getGameData();
 
-    const title = document.getElementById("title");
-    const devname = document.getElementById("dev-name");
-    const return_button = document.getElementById("game-page");
-    const file_name = document.getElementById("file-name");
-    const nav_title = document.getElementById("navigation-title");
+  const title = document.getElementById("title");
+  const devname = document.getElementById("dev-name");
+  const return_button = document.getElementById("game-page");
+  const file_name = document.getElementById("file-name");
+  const nav_title = document.getElementById("navigation-title");
 
-    title.textContent = `Download: "${gameData.name}"`;
-    nav_title.textContent = `Download ${gameData.name}`;
-    devname.textContent = `By: ${gameData.metadata.developer_name}`;
-    file_name.textContent = `${gameData.metadata.file_name} (${gameData.metadata.size} MB)`;
-    return_button.setAttribute("href", `game.html?g=${priceId}`);
+  title.textContent = `Download: "${gameData.name}"`;
+  nav_title.textContent = `Download ${gameData.name}`;
+  devname.textContent = `By: ${gameData.metadata.developer_name}`;
+  file_name.textContent = `${gameData.metadata.file_name} (${gameData.metadata.size} MB)`;
+  return_button.setAttribute("href", `game.html?g=${priceId}`);
 }
 
 if (gameIdParam && priceIdParam) {
