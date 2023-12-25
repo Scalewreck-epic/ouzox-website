1e14cd8c42a368240b14c73e4bc8ec176e4c2721
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-12-25 15:05:49 -0600

    remove sanitize url

--- assets/js/game/util/textEditor.js
+++ assets/js/game/util/textEditor.js
@@ -55,7 +55,6 @@ function createLink() {
   const url = prompt("Enter the link URL:");
 
   if (url) {
-    const cleanURL = sanitizeUrl(url);
     const selection = window.getSelection();
     const selectedText = selection.toString();
 
