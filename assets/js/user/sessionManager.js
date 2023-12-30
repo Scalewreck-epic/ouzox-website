638d55b078cba3c9d125bf6d60d1a661456207e1
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2023-12-30 17:44:07 -0600

    updated fetching cookie

--- assets/js/user/sessionManager.js
+++ assets/js/user/sessionManager.js
@@ -21,7 +21,7 @@ async function change_session_data(headers, endpoint) {
     error_label.textContent = result.Result.message;
   } else {
     error_label.textContent = "Error changing session data.";
-    console.error(`Unable to change session data: ${result.Result}`);
+    throw new Error(`Unable to change session data: ${result.Result}`);
   }
 }
 
@@ -31,78 +31,86 @@ export function fetch_cookie(wanted) {
     .map((pair) => pair.trim().split("="));
   const matchingCookie = cookiePairs.find(([name]) => name === wanted);
 
-  return matchingCookie
-    ? { Data: matchingCookie[1], Valid: true }
-    : { Data: "no data", Valid: false };
+  if (matchingCookie) {
+    return { Data: matchingCookie[1], Valid: true};
+  } else {
+    return { Data: null, Valid: false};
+  }
 }
 
 export async function change_email_data() {
-  const data = fetch_cookie("session_id");
+  const session_id = fetch_cookie("session_id").Data;
 
-  const new_email = document.getElementById("email_input").value;
-  const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(new_email);
-
-  if (data.Valid && validEmail) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: JSON.stringify({
-        session_id: data.Data,
-        new_email: new_email,
-      }),
-    };
-
-    await change_session_data(requestOptions, `${edit_user_email}${data.Data}`);
+  if (session_id) {
+    const new_email = document.getElementById("email_input").value;
+    const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(new_email);
+  
+    if (validEmail) {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+  
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify({
+          session_id: session_id,
+          new_email: new_email,
+        }),
+      };
+  
+      await change_session_data(requestOptions, `${edit_user_email}${session_id}`);
+    }
   }
 }
 
 export async function change_password_data() {
-  const data = getCookie("session_id");
+  const session_id = getCookie("session_id").Data;
 
-  const new_password = document.getElementById("password_input").value;
-  const old_password = document.getElementById("old_password_input").value;
-
-  const validPassword = new_password.length >= 8;
-
-  if (data.Valid && validPassword) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: JSON.stringify({
-        session_id: data.Data,
-        old_password: old_password,
-        new_password: new_password,
-      }),
-    };
-
-    await change_session_data(requestOptions, `${edit_user_pass}${data.Data}`);
+  if (session_id) {
+    const new_password = document.getElementById("password_input").value;
+    const old_password = document.getElementById("old_password_input").value;
+    const validPassword = new_password.length >= 8;
+  
+    if (validPassword) {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+  
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify({
+          session_id: session_id,
+          old_password: old_password,
+          new_password: new_password,
+        }),
+      };
+  
+      await change_session_data(requestOptions, `${edit_user_pass}${session_id}`);
+    }
   }
 }
 
 export async function change_status_data() {
-  const data = fetch_cookie("session_id");
+  const session_id = fetch_cookie("session_id").Data;
 
-  if (data.Valid) {
+  if (session_id) {
     const new_status = document.getElementById("status-input").value;
-
+  
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
+  
     const requestOptions = {
       method: "POST",
       headers: myHeaders,
       body: JSON.stringify({
-        session_id: data.Data,
+        session_id: session_id,
         status: new_status,
       }),
     };
-
+  
     await change_session_data(
       requestOptions,
-      `${edit_user_status}${data.Data}`
+      `${edit_user_status}${session_id}`
     );
   }
 }
@@ -132,34 +140,29 @@ export async function fetch_alternative_user(user_id) {
 }
 
 export async function fetch_user() {
-  const data = fetch_cookie("session_id");
+  const session_id = fetch_cookie("session_id").Data;
 
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const get_user_options = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
-  if (data.Valid) {
-    async function get_user() {
-      const result = await request(
-        `${get_user_with_session}${data.Data}`,
-        get_user_options,
-        true,
-        "get user (session)"
-      );
-
-      if (result.Success) {
-        return result.Result;
-      } else {
-        throw new Error(`Unable to get user with session ID: ${result.Result}`);
-      }
+  if (session_id) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  
+    const get_user_options = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+  
+    const result = await request(
+      `${get_user_with_session}${session_id}`,
+      get_user_options,
+      true,
+      "get user (session)"
+    );
+  
+    if (result.Success) {
+      return result.Result;
+    } else {
+      throw new Error(`Unable to get user with session ID: ${result.Result}`);
     }
-
-    const user = await get_user();
-    return user;
   }
 }
