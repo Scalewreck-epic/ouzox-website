f845d56996d56a0127e4d37db8d0e15836dd11ce
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2024-02-04 16:02:24 -0600

    changed error messages

--- assets/js/other/errorhandling.js
+++ assets/js/other/errorhandling.js
@@ -12,44 +12,46 @@ navigation_title.textContent = `Ouzox | Error ${error_code}`;
 switch (error_code) {
   case 400:
     message_header.textContent = "Bad Request";
-    message.textContent = "Your request was invalid.";
+    message.textContent =
+      "Your request didn't compute correctly. Please check your input and try again.";
     break;
   case 401:
     message_header.textContent = "Unauthorized";
-    message.textContent = "Your request was unprotected.";
+    message.textContent =
+      "Your request was unauthroized. Incorrect credentials used.";
     break;
   case 403:
     message_header.textContent = "Access Denied";
-    message.textContent = "You do not have permission to view this page.";
+    message.textContent = "You do not have permission to access this content.";
     break;
   case 404:
     message_header.textContent = "Not Found";
-    message.textContent = "Page cannot be found or no longer exists.";
+    message.textContent =
+      "You've wandered too far from the website. The page you're looking for isn't here.";
     break;
   case 405:
     message_header.textContent = "Method Not Allowed";
-    message.textContent = "Your request could not process the method.";
+    message.textContent = "Your request was using an invalid method.";
     break;
   case 409:
     message_header.textContent = "Conflict";
-    message.textContent =
-      "Your request could not be completed due to a conflict.";
+    message.textContent = "There's a conflict in your request.";
     break;
   case 429:
     message_header.textContent = "Too Many Requests";
-    message.textContent = "Your request has been called too much.";
+    message.textContent = "Just wait a few seconds then return back.";
     break;
   case 500:
     message_header.textContent = "Internal Server Error";
-    message.textContent =
-      "Your request was could not be completed due to an internal server error.";
+    message.textContent = "Ouzox is trying to fix the error in the server.";
     break;
   case 503:
     message_header.textContent = "Service Unavailable";
-    message.textContent = "Ouzox is currently unavailable.";
+    message.textContent =
+      "Ouzox is temporarily under maintenance. Please come back later.";
     break;
   default:
-    message_header.textContent = "Error";
-    message.textContent = "An unknown error occured.";
+    message_header.textContent = "Anomaly Error.";
+    message.textContent = "An unknown error has disturbed the site.";
     break;
 }
