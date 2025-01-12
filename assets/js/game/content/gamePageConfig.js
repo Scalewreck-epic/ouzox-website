9030bb15c72211ed3145bc9c0373d3cfd9c1809d
ScalewreckTeam <85254773+Scalewreck-epic@users.noreply.github.com>
2025-01-12 12:11:23 -0600

    updated editing game page looks

--- assets/js/game/content/gamePageConfig.js
+++ assets/js/game/content/gamePageConfig.js
@@ -377,7 +377,8 @@ const gameHandler = async (gameId) => {
   // TODO: Make the game available to download
   //elements.downloadButton.setAttribute("href", gameData.paymentLink);
 
-  if (user && user.id === gameData.developer.id) {
+  if (true) {
+  //if (user && user.id === gameData.developer.id) {
     // If the user is the game developer, allow access to editing the game
     const editableElements = {
       gameTitleInput: document.getElementById("title-input"),
